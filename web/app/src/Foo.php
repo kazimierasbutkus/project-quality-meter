7cffb77e4ff7747bbd6a13e6b2776ed2b58861b1
Kazimieras Butkus <kazimieras.butkus@nfq.lt>
2018-02-04 02:59:46 +0200

    mongodb basic script added

--- web/app/src/Foo.php
+++ web/app/src/Foo.php
@@ -2,10 +2,47 @@
 
 namespace App\Acme;
 
+use MongoDB\Client;
+
 class Foo
 {
     public function getName()
     {
+//        $manager = new MongoDB\Driver\Manager("mongodb://mongo:27017/test");
+//
+//        $bulk = new MongoDB\Driver\BulkWrite();
+//        $bulk->insert(['name' => 'John Doe']);
+//
+//        $writeConcern = new MongoDB\Driver\writeConcern(MongoDB\Driver\WriteConcern::MAJORITY, 100);
+//        $result = $manager->executeBulkWrite('test.mycollection', $bulk);
+//
+//        var_dump($result);
+
+        $client = new Client('mongodb://mongo:27017/test');
+        $seedData = array(
+            array(
+                'decade' => '1970s',
+                'artist' => 'Debby Boone',
+                'song' => 'You Light Up My Life',
+                'weeksAtOne' => 10
+            ),
+            array(
+                'decade' => '1980s',
+                'artist' => 'Olivia Newton-John',
+                'song' => 'Physical',
+                'weeksAtOne' => 10
+            ),
+            array(
+                'decade' => '1990s',
+                'artist' => 'Mariah Carey',
+                'song' => 'One Sweet Day',
+                'weeksAtOne' => 16
+            ),
+        );
+
+        $songs = $client->db->songsA;
+        $songs->insertMany($seedData);
+
         return 'Nginx PHP Mongo';
     }
 }
